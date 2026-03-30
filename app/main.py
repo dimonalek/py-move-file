403b535abc4c171aad1fc512c06b0a7b066d404f
dimonalek <dimonalek@gmail.com>
2026-03-30 16:22:43 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    try:
+        trash, source, destination = command.split()
+    except ValueError:
+        return
+    if trash != "mv" or not source or not destination:
+        return
+    if source == destination:
+        return
+    if "/" not in destination:
+        os.rename(source, destination)
+    else:
+        destination_dir = destination.split("/")
+        destination_file = destination_dir[-1]
+        destination_dir.pop()
+        destination_dir = "/".join(destination_dir)
+        if not os.path.exists(destination_dir):
+            os.makedirs(destination_dir)
+        with open(
+            source, "r"
+        ) as source_file, open(
+            destination, "w"
+        ) as destination_file:
+            for line in source_file:
+                destination_file.write(line)
+        os.remove(source)
